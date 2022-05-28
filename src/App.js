2944640e41a016ccebfeb8b11cc4c0a82ca15dfd
Shourov98hassan <kamruzzamanshourov98@gmail.com>
2022-05-28 00:09:44 -0700

    add to cart functionality added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
+      <Cart></Cart>
    
     </div>
   );
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Cart = () => {
+    const [cart,setCart] = useState(1);
+
+    const cartDecrease = () =>{
+        const cartMinus = cart - 1;
+        setCart(cartMinus);
+        if(cartMinus==0){
+            alert("you have to select at least one item!");
+            setCart(cart);
+        }
+    };
+
+    const cartIncrease =()=>{
+        const cartPlus = cart + 1;
+        setCart(cartPlus);
+        if(cartPlus==6){
+            alert("you can't select more than 5 items!!");
+            setCart(cart);
+        }
+    }
     return (
         <div>
-            
+                        
+<h3>Add To Cart</h3>
+            <h4>Add To Cart Item: {cart}</h4>
+            <div className="btn-item">
+            <button className='btn' onClick={cartDecrease}>-</button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button className='btn' onClick={cartIncrease}>+</button>
+            </div>
         </div>
     );
 };
